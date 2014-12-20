2fdf97816d2f4229fe08f57821b797381ae25e04
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-20 10:59:27 -0500

    Rendered the scene from an object.

--- scripts/Frame.js
+++ scripts/Frame.js
@@ -1,13 +1,11 @@
 var Scene = require("./Scene")
 var Menu = require("./Menu")
-var Dialogue = require("./Dialogue")
 
 var Frame = React.createClass({
     render: function() {
         return (
             <div id="frame">
                 <Scene/>
-                <Dialogue/>
                 <Menu/>
             </div>
         )
--- scripts/Scene.js
+++ scripts/Scene.js
@@ -1,7 +1,39 @@
+var first_scene = {
+    "mephistopheles": {
+        style: {
+            width: 50,
+            height: 75,
+            top: 295,
+            left: 505,
+            position: "absolute",
+            backgroundColor: "black"
+        },
+        onClick: function() {
+            console.log("i am clicked");
+        }
+    }
+}
+
 var Scene = React.createClass({
+    getInitialState: function() {
+        return {
+            "hello": "world"
+        }
+    },
     render: function() {
+        var scene = first_scene
+        var actors = new Array()
+        for(var actor in scene) {
+            actors.push(
+                <div key={actor}
+                     style={scene[actor].style}
+                     onClick={scene[actor].onClick}>
+                </div>
+            )
+        }
         return (
             <div className="scene">
+                {actors}
             </div>
         )
     }
