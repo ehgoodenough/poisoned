ce2641b4a25cb906bde1f85adc4efbbc5e0d957a
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-20 11:21:27 -0500

    Set up a store to manipulate the scene.

--- scripts/Scene.js
+++ scripts/Scene.js
@@ -1,27 +1,50 @@
-var first_scene = {
-    "mephistopheles": {
-        style: {
-            width: 50,
-            height: 75,
-            top: 295,
-            left: 505,
-            position: "absolute",
-            backgroundColor: "black"
-        },
-        onClick: function() {
-            console.log("i am clicked");
+var SceneData = {
+    "1.0": {
+        "mephistopheles": {
+            style: {
+                top: 8.5 + "rem",
+                left: 14.25 + "rem",
+                width: 1.25 + "rem",
+                height: 2 + "rem",
+                position: "absolute",
+                backgroundColor: "black"
+            },
+            onClick: function() {
+                //SceneActions.ChangeIndex("1.1")
+            }
         }
     }
 }
 
-var Scene = React.createClass({
-    getInitialState: function() {
-        return {
-            "hello": "world"
-        }
+var SceneActions = Reflux.createActions([
+    "ChangeIndex"
+])
+
+var SceneStore = Reflux.createStore({
+    listenables: [
+        SceneActions
+    ],
+    onChangeIndex: function(index) {
+        this.state.index = index
+        this.trigger(this.state)
     },
+    state: {
+        index: "1.0"
+    },
+    getInitialState: function() {
+        return this.state
+    }
+})
+
+var Scene = React.createClass({
+    mixins: [
+        Reflux.connect(SceneStore)
+    ],
     render: function() {
-        var scene = first_scene
+        var scene = SceneData[this.state.index]
+        console.log(scene)
+        
+        
         var actors = new Array()
         for(var actor in scene) {
             actors.push(
